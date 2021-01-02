e9bcee1ca4df32b3e0ecf764e6e34b62c043a558
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-02 10:25:32 -0700

    Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.peliculasEnCines = [
+      {
+        titulo: 'Spider-Man',
+      fechaLanzamiento: new Date(),
+      precio: 1400.56
+      },
+      {
+        titulo: 'Mohana',
+      fechaLanzamiento: new Date('2017-05-13'),
+      precio: 1400.56
+      },
+    ]
+  }
+  peliculasEnCines;
+  peliculasProximosEstrenos = [
+    {
+      titulo: 'Spider-Man',
+    fechaLanzamiento: new Date(),
+    precio: 1400.56
+    },
+    {
+      titulo: 'Mohana',
+    fechaLanzamiento: new Date('2017-05-13'),
+    precio: 1400.56
+    },
+    {
+      titulo: 'Mohana II',
+    fechaLanzamiento: new Date('2017-05-13'),
+    precio: 1400.56
+    },
+  ];
   title = 'front-end';
+
+  manejarRated(voto: number): void {
+    alert(voto);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
+import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from '../app/material/material.module';
+import { MenuComponent } from './menu/menu.component';
+import { RatingComponent } from './utilidades/rating/rating.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoPeliculasComponent,
+    ListadoGenericoComponent,
+    MenuComponent,
+    RatingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
