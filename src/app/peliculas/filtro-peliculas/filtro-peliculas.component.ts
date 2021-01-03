58075ef3425ae6628218cc496f565281364bee67
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-02 23:25:21 -0700

    Logica del filtro

--- src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
+++ src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
@@ -23,17 +23,43 @@ export class FiltroPeliculasComponent implements OnInit {
     { titulo: 'Moana', enCines: true, proximosEstrenos: false, generos: [3], poster: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_UX182_CR0,0,182,268_AL_.jpg'},
     { titulo: 'Fas & Furious', enCines: false, proximosEstrenos: false, generos: [1,3], poster: 'https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UY268_CR1,0,182,268_AL_.jpg'},
   ];
+
+  peliculasOriginal = this.peliculas;
+
+  formularioOriginal = {
+    titulo: '',
+    generoId: 0,
+    proximosEstrenos: false,
+    enCines: false
+  }
   ngOnInit(): void {
-    this.formBuilder.group({
-      titulo: '',
-      generoId: 0,
-      proximosEstrenos: false,
-      enCines: false
-    });
+    this.form = this.formBuilder.group(this.formularioOriginal);
+
+    this.form.valueChanges
+      .subscribe(valores => {
+        this.peliculas = this.peliculasOriginal;
+        this.buscarPeliculas(valores);
+      });
+
+  }
+
+  buscarPeliculas(valores: any){
+    if (valores.titulo) {
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.titulo.indexOf(valores.titulo) !== -1)
+    }
+    if (valores.generoId !==0) {
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.generos.indexOf(valores.generoId) !== -1);
+    }
+    if (valores.proximosEstrenos) {
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.proximosEstrenos);
+    }
+    if (valores.enCines) {
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.enCines);
+    }
   }
 
   limpiar() {
-
+    this.form.patchValue(this.formularioOriginal);
   }
 
 }
