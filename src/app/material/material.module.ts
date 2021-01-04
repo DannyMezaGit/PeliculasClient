dcd400b6da256573db365c52e21f7df2a904f4e0
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-04 11:52:20 -0700

    preparar formulario para nuevos autores

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,6 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
 
 
 
@@ -25,6 +27,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatInputModule,
     MatSelectModule,
     MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
   ]
     
   
