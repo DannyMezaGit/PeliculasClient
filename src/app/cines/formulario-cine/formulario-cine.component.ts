585da43082fc495e624c9dac517cfb6817145b9c
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-04 19:31:39 -0700

    funcionalidad de crear cine

---
+++ src/app/cines/formulario-cine/formulario-cine.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cineCreacionDTO } from '../cine';
+
+@Component({
+  selector: 'app-formulario-cine',
+  templateUrl: './formulario-cine.component.html',
+  styleUrls: ['./formulario-cine.component.css']
+})
+export class FormularioCineComponent implements OnInit {
+
+  constructor( private formBuilder: FormBuilder) { }
+
+  @Input()
+  modelo: cineCreacionDTO;
+
+  @Output()
+  guardarCambios: EventEmitter<cineCreacionDTO> = new EventEmitter<cineCreacionDTO>();
+  form: FormGroup;
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nombre: ['', {validators: [Validators.required]}]
+    });
+  }
+
+  OnSubmit() {
+    this.guardarCambios.emit(this.form.value);
+  }
+
+}
