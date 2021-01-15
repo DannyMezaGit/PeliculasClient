7c0611f466e602d29ee5951b2352b847c538780a
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-15 12:24:03 -0700

    Selector de actores - componente tabla

--- src/app/actores/autocomplete-actores/autocomplete-actores.component.ts
+++ src/app/actores/autocomplete-actores/autocomplete-actores.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-autocomplete-actores',
@@ -13,14 +14,18 @@ export class AutocompleteActoresComponent implements OnInit {
 
   control: FormControl = new FormControl();
   actores = [
-    {nombre: 'Tom Holland', foto: 'https://m.media-amazon.com/images/M/MV5BNTAzMzA3NjQwOF5BMl5BanBnXkFtZTgwMDUzODQ5MTI@._V1_UY317_CR23,0,214,317_AL_.jpg'},
-    {nombre: 'Tom Hanks', foto: 'https://m.media-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg'},
-    {nombre: 'Samuel L Jackson', foto: 'https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg'},
+    {nombre: 'Tom Holland', personaje: '', foto: 'https://m.media-amazon.com/images/M/MV5BNTAzMzA3NjQwOF5BMl5BanBnXkFtZTgwMDUzODQ5MTI@._V1_UY317_CR23,0,214,317_AL_.jpg'},
+    {nombre: 'Tom Hanks', personaje: '', foto: 'https://m.media-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg'},
+    {nombre: 'Samuel L Jackson', personaje: '', foto: 'https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg'},
   ]
 
   actoresOriginal = this.actores;
 
   actoresSeleccionados = [];
+
+  columnasAMostrar = ['imagen', 'nombre', 'personaje', 'acciones'];
+
+  @ViewChild(MatTable) table: MatTable<any>;
   ngOnInit(): void {
     this.control.valueChanges.subscribe(valor => {
       this.actores = this.actoresOriginal;
@@ -32,6 +37,15 @@ export class AutocompleteActoresComponent implements OnInit {
     console.log(event.option.value);
     this.actoresSeleccionados.push(event.option.value);
     this.control.patchValue('');
+    if(this.table !== undefined) {
+      this.table.renderRows();
+    }
+  }
+
+  eliminar(actor){
+    const indice = this.actoresSeleccionados.findIndex(a => a.nombre == actor.nombre);
+    this.actoresSeleccionados.splice(indice, 1);
+    this.table.renderRows();
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,7 @@ import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatTableModule } from "@angular/material/table";
 
 
 
@@ -32,6 +33,7 @@ import { MatAutocompleteModule } from "@angular/material/autocomplete";
     MatNativeDateModule,
     MatTabsModule,
     MatAutocompleteModule,
+    MatTableModule,
     
   ]
     
